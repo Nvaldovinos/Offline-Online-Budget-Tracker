58098c79d49eca1ae76b704e36e28a88c2610798
Naomi <n.valdovinos74@gmail.com>
2021-08-11 08:00:49 -0400

    added files to public folder, and finished manifest file

---
+++ public/db.js
@@ -0,0 +1,10 @@
+//create an indexdb variable
+const indexedDB = window.indexedDB;
+
+//add index values from window object 
+//set an empty variable to keep track db 
+
+let db;
+
+//declare variable for request 
+const 
---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const FILES_TO_CACHE =[
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/db.js',
+    '/index.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/style.css'
+];
+
+
+//add evenlistener using self keyword.
